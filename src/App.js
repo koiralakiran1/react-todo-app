f81cc3d9aae7200b6ea8018c19be0dada16e713d
Kiran Koirala <koirala.kiran1@gmail.com>
2019-01-10 22:40:22 +0545

    Add a static html-mockup  with no functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ class App extends Component {
     return (
       <div className="App">
         {/* <button className='btn btn-secondary'>Hello</button> */}
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">All</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Completed</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Remaining</a>
+          </li>
+        </ul>
+        <div className="add_item_bar form-group">
+          <input type="text" className="form-control" placeholder="Enter new TODO item" />
+        </div>
+        <ul className="list-group">
+          <li className="list-group-item">Something</li>
+          <li className="list-group-item">Something else</li>
+          <li className="list-group-item">Something something else</li>
+        </ul>
       </div>
     );
   }
