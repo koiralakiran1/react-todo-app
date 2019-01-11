8533b4cf2baf7577b59faa6807578d0eeb6bc5ba
Kiran Koirala <koirala.kiran1@gmail.com>
2019-01-11 17:57:17 +0545

    Add list item component with remove functionality in buttons

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { ToDoList } from './components/ToDoList';
-import { Input } from './components/Input';
-import { Navigation } from './components/Navigation';
+import { ToDoApp } from './components/ToDoApp';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTodo: '',
-      allToDoArray: []
-    };
-    this.handleOnChange = this.handleOnChange.bind(this);
-    this.handleOnSubmit = this.handleOnSubmit.bind(this);
-  }
-
-  handleOnChange(e) {
-    this.setState({
-      currentTodo: e.target.value
-    });
-  }
-
-  handleOnSubmit(e) {
-    e.preventDefault();
-    const newToDO = this.state.allToDoArray;
-    newToDO.push(this.state.currentTodo);
-
-    this.setState({
-      allToDoArray: newToDO,
-      currentTodo: ''
-    });
-  }
-
   render() {
     return (
-      <div className="App">
-        {/* <button className='btn btn-secondary'>Hello</button> */}
-        <Navigation items={['All', 'Completed', 'Remaining']} />
-        <Input value={this.state.currentTodo} type='text' placeholder="Add New TODO" onChange={this.handleOnChange} onSubmit={this.handleOnSubmit}/>
-        <ToDoList items={this.state.allToDoArray} />
+      <div className='App'>
+        <ToDoApp />
       </div>
     );
   }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { ToDoListItem } from './ToDoListItem';
 
 export const ToDoList = (props) =>
   (
     <ul className="list-group">
-      {props.items.map((item, i) => <li key={'index_' + i} className="list-group-item todo_list_item">{item}</li>)}
+      {props.items.map((item, i) =>
+        <ToDoListItem
+          value={item}
+          key={i}
+          myKey={i}
+          onDelete={props.onDelete}
+          onDone={props.onDone}
+          onEdit={props.onEdit} />
+      )}
     </ul>
   );
 
