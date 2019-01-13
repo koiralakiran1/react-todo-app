17090ae3eb33319fe35a046a12fd5c28cff84fdc
Kiran Koirala <koirala.kiran1@gmail.com>
2019-01-14 01:44:53 +0545

    Add tabs on click options

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,11 @@ export const Navigation = (props) =>
     <ul className="nav nav-fill nav-tabs">
       {props.items.map((item, i) =>
         (
-          <li key={'index_' + i} className="nav-item">
-            <a className="nav-link" href="/">{item}</a>
+          <li key={'index_' + i} className='nav-item'>
+            <a
+              onClick={(i === 0) ? props.displayAllTodos : ((i === 1) ? props.displayCompletedTodos : props.displayRemainingTodos)}
+              className={(props.currentList === i) ? 'nav-link active' : 'nav-link'}
+              href="#">{item}</a>
           </li>
         )
       )}
