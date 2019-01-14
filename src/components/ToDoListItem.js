96b414249f81de1f09b63bc1929fa8fe76fd29dd
Kiran Koirala <koirala.kiran1@gmail.com>
2019-01-14 08:00:02 +0545

    Fix a bug by using Date.now() as unique id

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ToDoListItem = (props) => {
   if(props.value.editingStatus) {
     return (
-      <li className={props.value.doneStatus ? 'done_task_item list-group-item todo_list_item' : 'list-group-item todo_list_item'} key={'item_' + props.myKey} id={props.myKey}>
+      <li id={props.value.id} className={props.value.doneStatus ? 'done_task_item list-group-item todo_list_item' : 'list-group-item todo_list_item'} key={'item_' + props.myKey}>
         <form className='list_item_edit_form' onSubmit={props.onEditSubmit}>
           <input type='text' id={'input_' + props.myKey} onChange={props.onEditingChange} value={props.value.todoContent} name={'input_' + props.myKey}  />
           <p><em>Press Enter To Save</em></p>
@@ -12,7 +12,7 @@ export const ToDoListItem = (props) => {
     );
   } else {
     return (
-      <li className={props.value.doneStatus ? 'done_task_item list-group-item todo_list_item' : 'list-group-item todo_list_item'} key={'item_' + props.myKey} id={props.myKey}>
+      <li className={props.value.doneStatus ? 'done_task_item list-group-item todo_list_item' : 'list-group-item todo_list_item'} key={'item_' + props.myKey} id={props.value.id}>
         <div className='list_item_display_text'>
           <input type='checkbox' checked={props.value.doneStatus ? 'checked' : ''} onChange={props.handleCheckBoxChange} id={'input_' + props.myKey} value={'input_' + props.myKey} name={'input_' + props.myKey}  />
           <label className={props.value.doneStatus ? 'done_task_text' : ''} htmlFor={'input_' + props.myKey}>{props.value.todoContent}</label>
